4f56dec32adbe8271b0368cc61b5903d280f0710
MatheusJanibelli <janibellim@gmail.com>
2021-03-11 16:23:44 -0300

    Add password safety checker

--- login_functions.py
+++ login_functions.py
@@ -39,14 +39,34 @@ def input_new_username():
     return new_user
 
 
+def check_password(password):
+    """Checks if the password is safe enough (At least 1 number, 1 capital letter and 1 special character)."""
+    cap_letter_regex = re.compile(r"[A-Z]+")
+    cap_letter_found = re.search(cap_letter_regex, password)
+
+    number_regex = re.compile(r"\d+")
+    number_found = re.search(number_regex, password)
+
+    spec_charac_regex = re.compile(r"\W+")
+    spec_charac_found = re.search(spec_charac_regex, password)
+
+    if cap_letter_found and number_found and spec_charac_found:
+        return True
+    else:
+        return False
+
 def input_new_password():
     """Allows the password input and checks if it's valid or not."""
     new_password = input("Type your password: ")
-    password_check = input("Type your password again: ")
-    while password_check != new_password: 
-        print("Passwords doesn't match! Try again.")
+    if check_password(new_password):
+        password_check = input("Type your password again: ")
+        while password_check != new_password: 
+            print("Passwords doesn't match! Try again.")
+            return input_new_password()
+        return new_password
+    else:
+        print("Your password needs to be stronger! (At least 1 number, 1 capital letter and 1 special character)")
         return input_new_password()
-    return new_password
         
 
 def signup():
