5cdf298d62ca13a63325564226758604518832fd
MatheusJanibelli <janibellim@gmail.com>
2021-03-12 15:29:42 -0300

    Fix empty database bug when logging

--- login.py
+++ login.py
@@ -6,7 +6,12 @@ check = input("Do you have an account?[y/n]: ")
 while check[0].lower() != "y" and check[0].lower() != "n":
     print("This option does not exist! Try again...")
     check = input("Do you have an account?[y/n]: ")
+
 if check[0].lower() == "y":
-    login_functions.signin()
+    if login_functions.load_users():
+        login_functions.signin()
+    else:
+        print("No users have been saved yet. Try creating a new account.")
+        login_functions.signup()
 if check[0].lower() == "n":
     login_functions.signup()
