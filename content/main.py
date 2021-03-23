1b6e145951f2eccb41ccfde5664c1cfe10cd40ac
MatheusJanibelli <janibellim@gmail.com>
2021-03-23 17:08:46 -0300

    Add content file

---
+++ content/main.py
@@ -0,0 +1,17 @@
+import signup_signin, load_save
+
+print(" Welcome to Python Login! ".center(48, "="))
+check = input("Do you have an account?[y/n]: ")
+
+while check[0].lower() != "y" and check[0].lower() != "n":
+    print("This option does not exist! Try again...")
+    check = input("Do you have an account?[y/n]: ")
+
+if check[0].lower() == "y":
+    if load_save.load_users():
+        signup_signin.signin()
+    else:
+        print("No users have been saved yet. Try creating a new account.")
+        signup_signin.signup()
+if check[0].lower() == "n":
+    signup_signin.signup()
